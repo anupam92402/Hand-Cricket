9aeca469f4cdd5d6538206a67cc6828c0687d4be
ANUPAM GUPTA <anupamg2001@gmail.com>
2025-04-05 23:50:19 +0530

    overlay added while navigation

--- lib/presentation/widgets/game_screen/overlay_screen.dart
+++ lib/presentation/widgets/game_screen/overlay_screen.dart
@@ -3,23 +3,23 @@ import 'package:hand_cricket/utils/app_colors.dart';
 import 'dart:async';
 
 class OverlayUtils {
-  static void showImageOverlay({required BuildContext context,required Widget widget}) {
+  static Future<void> showImageOverlay({
+    required BuildContext context,
+    required Widget widget,
+    Duration duration = const Duration(seconds: 2),
+  }) async {
     final overlay = Overlay.of(context);
     final overlayEntry = OverlayEntry(
       builder: (_) => Stack(
         children: [
           Container(color: AppColors.color000000.withOpacity(0.7)),
-          Center(
-            child:widget,
-          ),
+          Center(child: widget),
         ],
       ),
     );
-
     overlay.insert(overlayEntry);
+    await Future.delayed(duration);
+    overlayEntry.remove();
 
-    Timer(const Duration(seconds: 2), () {
-      overlayEntry.remove();
-    });
   }
 }
--- lib/presentation/widgets/rules_screen/bottom_button_widget.dart
+++ lib/presentation/widgets/rules_screen/bottom_button_widget.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:hand_cricket/utils/routes_name.dart';
 
 import '../../../utils/app_colors.dart';
+import '../../../utils/assets_path.dart';
 import '../../../utils/string_constants.dart';
+import '../game_screen/overlay_screen.dart';
 
 class BottomButtonWidget extends StatelessWidget {
   const BottomButtonWidget({super.key});
@@ -11,8 +13,16 @@ class BottomButtonWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return InkWell(
-      onTap: (){
-        Navigator.pushNamed(context, RoutesName.gameScreen);
+      onTap: () async {
+        await OverlayUtils.showImageOverlay(
+            context: context,
+            widget: Image.asset(
+              AssetsPath.batting,
+              fit: BoxFit.contain,
+            ));
+        if (context.mounted) {
+          Navigator.pushNamed(context, RoutesName.gameScreen);
+        }
       },
       child: Container(
         width: size.width * 0.75,
